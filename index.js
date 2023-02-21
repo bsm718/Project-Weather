df3496d4ce02c6b5b1ca0c8b6307069b22bab61b
bsm718 <124943949+bsm718@users.noreply.github.com>
2023-02-21 15:03:46 -0800

    Updated
    -updated API with shecodes API
    -started forecast API
    -updated css for when screen size is adjusted

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function currentDate(now) {
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   let day = days[now.getDay()];
 
@@ -44,27 +44,39 @@ let todayTime = document.querySelector("#current-time");
 todayTime.innerHTML = currentTime(now);
 //
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "9f08e2dtc6459be5bd4d439a047ao3cb";
+  let lat = coordinates.latitude;
+  let lon = coordinates.longitude;
+
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
+//
 function showTemperature(response) {
-  let cityName = response.data.name;
+  console.log(response.data);
+  let cityName = response.data.city;
   let display = document.querySelector("#display");
   display.innerHTML = cityName;
 
-  let icon = response.data.weather[0].icon;
+  let icon = response.data.condition.icon;
   let weatherIcon = document.querySelector("#weather-icon");
   weatherIcon.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${icon}@2x.png`
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${icon}.png`
   );
-  weatherIcon.setAttribute("alt", response.data.weather[0].description);
+  weatherIcon.setAttribute("alt", response.data.condition.icon);
 
-  let temperature = Math.round(response.data.main.temp);
+  let temperature = response.data.temperature.current;
+  temperature = Math.round(temperature);
   let fDegrees = document.querySelector("#fahrenheit");
   fDegrees.innerHTML = temperature;
 
   let description = document.querySelector("#description");
-  description.innerHTML = response.data.weather[0].description;
+  description.innerHTML = response.data.condition.description;
 
-  let humPercent = response.data.main.humidity;
+  let humPercent = response.data.temperature.humidity;
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = humPercent;
 
@@ -72,27 +84,19 @@ function showTemperature(response) {
   let wind = document.querySelector("#wind");
   wind.innerHTML = windSpeed;
 
-  let tempMin = Math.round(response.data.main.temp_min);
-  let min = document.querySelector("#min");
-  min.innerHTML = tempMin;
-
-  let tempMax = Math.round(response.data.main.temp_max);
-  let max = document.querySelector("#max");
-  max.innerHTML = tempMax;
-
-  fahrenheitTemp = response.data.main.temp;
+  fahrenheitTemp = response.data.temperature.current;
   windy = response.data.wind.speed;
-  minTemp = response.data.main.temp_min;
-  maxTemp = response.data.main.temp_max;
+
+  getForecast(response.data.coordinates);
 }
 
 //
 function searchCity(cityInput) {
-  let apiKey = "65af86af583e025f3332a52aa4176f36";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=imperial`;
+  let apiKey = "9f08e2dtc6459be5bd4d439a047ao3cb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(showTemperature);
 }
-
+//
 function submitButton(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input").value;
@@ -104,10 +108,10 @@ function submitButton(event) {
 function searchLocation(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  let apiKey = "65af86af583e025f3332a52aa4176f36";
+  let apiKey = "9f08e2dtc6459be5bd4d439a047ao3cb";
   let units = "imperial";
 
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=${lat}&lon=${lon}&key=${apiKey}&units=${units}`;
 
   axios.get(apiUrl).then(showTemperature);
 }
@@ -139,17 +143,7 @@ function convertToCelsius(event) {
   w.innerHTML = " km/h";
 
   let fc = document.querySelector("#f-c");
-  fc.innerHTML = "°C";
-
-  let minElement = document.querySelector("#min");
-  let min = minElement.innerHTML;
-  min = Number(min);
-  minElement.innerHTML = Math.round(((minTemp - 32) * 5) / 9);
-
-  let maxElement = document.querySelector("#max");
-  let max = maxElement.innerHTML;
-  max = Number(max);
-  maxElement.innerHTML = Math.round(((maxTemp - 32) * 5) / 9);
+  fc.innerHTML = " °C";
 }
 
 function convertToFahrenheit(event) {
@@ -167,18 +161,42 @@ function convertToFahrenheit(event) {
   w.innerHTML = " mph";
 
   let fc = document.querySelector("#f-c");
-  fc.innerHTML = "°F";
-
-  let minElement = document.querySelector("#min");
-  minElement.innerHTML = Math.round(minTemp);
-
-  let maxElement = document.querySelector("#max");
-  maxElement.innerHTML = Math.round(maxTemp);
+  fc.innerHTML = " °F";
 }
+//
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = "";
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="futureForecast">${day}</div>
+                    <div class="futureIcon">
+                      <img
+                        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-night.png"
+                        alt="current weather icon"
+                        width="36px"
+                      />
+                    </div>
+
+                    <div class="futureMinMax">
+                      <span class="futureMin">50°</span>
+
+                      <span>/</span>
+                      <span class="futureMax">43°</span>
+                    </div>
+                    </div>
+                    </div>
+        `;
+  });
+
+  forecastElement.innerHTML = forecastHTML;
+}
+//
 let fahrenheitTemp = null;
 let windy = null;
-let minTemp = null;
-let maxTemp = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", submitButton);
@@ -190,4 +208,4 @@ let flink = document.querySelector("#f-link");
 flink.addEventListener("click", convertToFahrenheit);
 
 searchCity("Los Angeles");
-//
+displayForecast();
